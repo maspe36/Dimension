7180c528079ab17726235690556afe1d43a4da10
Sam Privett <privett.sam@gmail.com>
2018-11-11 00:14:04 -0500

    Added an empty card object for the game to begin experimenting with pybind11

---
+++ DimensionServer/src/Game/Card.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sam on 11/10/2018.
+//
+
+#include "Card.hpp"
+
+Dimension::Game::Card::Card(const std::string &name) : name(name)
+{
+
+}
---
+++ DimensionServer/src/Game/Card.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Sam on 11/10/2018.
+//
+
+#ifndef DIMENSIONSERVER_CARD_HPP
+#define DIMENSIONSERVER_CARD_HPP
+
+
+#include <memory>
+
+namespace Dimension
+{
+    namespace Game
+    {
+        class Card : public std::enable_shared_from_this<Card>
+        {
+        public:
+            std::string name;
+
+            explicit Card(const std::string& name);
+        };
+    }
+}
+
+
+#endif //DIMENSIONSERVER_CARD_HPP
